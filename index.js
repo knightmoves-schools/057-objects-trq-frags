8b9133dbbf03988eb56652e55830d4a08b1dbf43
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-21 16:59:38 +0000

    i forgot onje thing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let school = {
     'name': "Penjamin",
     'average class size': 7,
     mascot: Ben,
-}
+};
 
 let string = '"${name} has an average class size of ${average class size} and their mascot is a ${mascot}"'
 document.getElementById('result').innerHTML = string;

65f339fa9364c93d8d3061976737b701f0e8307b
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-21 16:58:03 +0000

    penjamin

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let school = {
+    'name': "Penjamin",
+    'average class size': 7,
+    mascot: Ben,
+}
 
+let string = '"${name} has an average class size of ${average class size} and their mascot is a ${mascot}"'
+document.getElementById('result').innerHTML = string;

4abf94d3132b74abbd8e1b8d463266b4a0e027d9
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-21 17:01:59 +0000

    class

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let school = {
-    'name': "Penjamin",
-    'average class size': 7,
-    mascot: Ben,
+    name: "Penjamin",
+    "average class size": 7,
+    mascot: "Ben",
 };
 
-let string = '"${name} has an average class size of ${average class size} and their mascot is a ${mascot}"'
+let string = `${school.name} has an average class size of ${school["average class size"]} and their mascot is a ${school.mascot}`;
 document.getElementById('result').innerHTML = string;
